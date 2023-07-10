ba529ce092dc834336c5a1cb9dc8e1b0d2291afe
pildrums <there7788@gmail.com>
2023-07-10 21:58:12 +0900

    feat: :sparkles: 차트 컴포넌트 작성 중

--- src/components/chart/Chart.tsx
+++ src/components/chart/Chart.tsx
@@ -1,5 +1,62 @@
+import ApexCharts from "react-apexcharts";
+import styled from "styled-components";
+
 function Chart() {
-  
+  return (
+    <ChartContainer>
+      <ApexCharts
+        series={[0, 10, 20, 30, 40]}
+        type="donut"
+        options={{
+          labels: ["a", "b", "c", "d", "e"],
+          dataLabels: {
+            enabled: true,
+            formatter: (val) => {
+              return val + "₩";
+            },
+          },
+          title: {
+            text: "title",
+          },
+          plotOptions: {
+            pie: {
+              donut: {
+                labels: {
+                  name: {
+                    show: true,
+                    formatter: (val) => {
+                      return val;
+                    },
+                  },
+                },
+              },
+            },
+          },
+        }}
+      />
+      <DummyBox />
+    </ChartContainer>
+  );
 }
+
+const ChartContainer = styled.div`
+  width: 100%;
+  padding: 20px 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 20px;
+  background: #fff;
+  border-radius: 15px;
+`;
+
+const DummyBox = styled.div`
+  width: 200px;
+  height: 200px;
+  background: blueviolet;
+  &:last-child {
+    background: goldenrod;
+  }
+`;
 
 export default Chart;
