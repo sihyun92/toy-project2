aada2da32e40e250f2deb99e54a7605d808a6a82
pildrums <there7788@gmail.com>
2023-07-12 19:25:50 +0900

    feat: :sparkles: QueryClientProvider 컴포넌트 추가

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,14 @@
 import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "react-query";
 import App from "./App";
+
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement,
 );
 root.render(
-  <>
+  <QueryClientProvider client={queryClient}>
     <App />
-  </>,
+  </QueryClientProvider>,
 );
