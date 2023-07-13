89d8a1e20f171408464a655ef6c359577196ac73
pildrums <there7788@gmail.com>
2023-07-14 01:21:01 +0900

    refactor: :hammer: MonthlyChart 컴포넌트 렌더링 및 기존 차트 코드 삭제

--- src/components/chart/Chart.tsx
+++ src/components/chart/Chart.tsx
@@ -1,48 +1,10 @@
-import { Dispatch, SetStateAction, useEffect, useState } from "react";
-import ApexCharts from "react-apexcharts";
-import { useMutation, useQuery } from "react-query";
 import styled from "styled-components";
-import { getPeriodConsume } from "../../lib/api/consumeAPI";
-
-interface IChartProps {
-  userId: string;
-  year: number;
-  month: number;
-}
+import MonthlyChart from "./MonthlyChart";
 
 function Chart() {
-  const [period, setPeriod] = useState("");
-  const [userId, setUserId] = useState("");
-  const { data, isLoading } = useQuery(
-    ["chart"],
-    () => getPeriodConsume(period, userId),
-    {
-      refetchInterval: 10000,
-    },
-  );
-  useEffect(() => {
-    getPeriodConsume("daily", "user123");
-    console.log(getPeriodConsume);
-  }, []);
-
   return (
     <ChartContainer>
-      <ApexCharts
-        series={[1, 2, 3, 4, 5]}
-        type="donut"
-        options={{
-          labels: ["a", "b", "c", "d", "e"],
-          dataLabels: {
-            enabled: true,
-            formatter: (val) => {
-              return val + "₩";
-            },
-          },
-          title: {
-            text: "title",
-          },
-        }}
-      />
+      <MonthlyChart />
       <DummyBox />
     </ChartContainer>
   );
