b134c87d843229f9413b93fd6a8c013e31e52e8b
pildrums <there7788@gmail.com>
2023-07-18 16:26:32 +0900

    feat: :sparkles: Chart 컴포넌트에서 상태관리 시작
    API 호출과 totalAmount/id 값 상태 관리

--- src/components/chart/Chart.tsx
+++ src/components/chart/Chart.tsx
@@ -1,10 +1,33 @@
+import { useEffect, useState } from "react";
 import styled from "styled-components";
+import { getPeriodConsume } from "../../lib/api/consumeAPI";
 import MonthlyChart from "./MonthlyChart";
 
+interface IExpense {
+  _id: string;
+  totalAmount: number;
+}
+
 function Chart() {
+  const [totalAmount, setTotalAmount] = useState<number[]>([]);
+  const [id, setId] = useState<string[]>([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const result = await getPeriodConsume("monthly", "team1");
+        setTotalAmount(result.data.map((item: IExpense) => item.totalAmount));
+        setId(result.data.map((item: IExpense) => item._id));
+        console.log(result.data);
+        console.log(result.data.map((item: IExpense) => item.totalAmount));
+      } catch (e) {
+        console.error(e, "오류 발생!");
+      }
+    };
+    fetchData();
+  }, []);
   return (
     <ChartContainer>
-      <MonthlyChart />
+      <MonthlyChart id={id} totalAmount={totalAmount} />
       <DummyBox />
     </ChartContainer>
   );
