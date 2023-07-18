63a6062d3d47b2bc38b58fc4367dad5c2002cc39
pildrums <there7788@gmail.com>
2023-07-18 16:29:23 +0900

    chore: :wrench: MonthlyChart 컴포넌트 데이터 받아오기 작업 중
    1. ApexCharts 컴포넌트에 데이터 받아오기 성공
    2. 받아온 금액 데이터 중 음수 값 처리 중

--- src/components/chart/MonthlyChart.tsx
+++ src/components/chart/MonthlyChart.tsx
@@ -1,33 +1,32 @@
-import { useEffect, useState } from "react";
-import { getPeriodConsume } from "../../lib/api/consumeAPI";
-import { Chart as ChartJS, ArcElement } from "chart.js";
+import ApexCharts from "react-apexcharts";
 
-ChartJS.register(ArcElement);
-
-interface IExpense {
-  _id: string;
-  totalAmount: number;
+// Interface
+interface MonthlyChartProps {
+  id: string[];
+  totalAmount: number[];
 }
 
 // Component
-function MonthlyChart() {
-  // hooks
-  const [amount, setAmount] = useState([]);
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const result = await getPeriodConsume("monthly", "team1");
-        setAmount(result.data.map((item: IExpense) => item.totalAmount));
-        console.log(result.data);
-      } catch (e) {
-        console.error(e, "오류 발생!");
-      }
-    };
-    fetchData();
-  }, []);
-
+function MonthlyChart({ id, totalAmount }: MonthlyChartProps) {
   // Render
-  return null;
+  return (
+    <ApexCharts
+      series={totalAmount}
+      type="donut"
+      options={{
+        labels: id,
+        dataLabels: {
+          enabled: true,
+          formatter: (val: number) => {
+            return `${Math.floor(val)}₩`;
+          },
+        },
+        title: {
+          text: "월별",
+        },
+      }}
+    />
+  );
 }
 
 export default MonthlyChart;
