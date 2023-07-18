89f9935c498afd2503bd7d596bd92dbb63900063
pildrums <there7788@gmail.com>
2023-07-19 04:02:44 +0900

    chore: :wrench: 차트 컴포넌트 리팩토링

--- src/components/chart/Chart.tsx
+++ src/components/chart/Chart.tsx
@@ -1,12 +1,35 @@
 import { useEffect, useState } from "react";
 import styled from "styled-components";
 import { getPeriodConsume } from "../../lib/api/consumeAPI";
+// import DailyChart from "./DailyChart";
 import MonthlyChart from "./MonthlyChart";
+// import WeeklyChart from "./WeeklyChart";
 
 interface IExpense {
   _id: string;
   totalAmount: number;
 }
+
+// interface IChartProps {
+//   type: string;
+// }
+
+// interface ITextMap {
+//   [key: string]: string;
+// }
+
+// Variable / Constant
+// const textMap: ITextMap = {
+//   monthly: "월별",
+//   weekly: "주별",
+//   daily: "일별",
+// };
+
+// const typeMap: ITextMap = {
+//   monthly: "monthly",
+//   weekly: "weekly",
+//   daily: "daily",
+// };
 
 function Chart() {
   const [totalAmount, setTotalAmount] = useState<number[]>([]);
@@ -27,7 +50,14 @@ function Chart() {
   }, []);
   return (
     <ChartContainer>
-      <MonthlyChart id={id} totalAmount={totalAmount} />
+      {/* <Tab>
+        <TabButton>일별</TabButton>
+        <TabButton>주별</TabButton>
+        <TabButton>월별</TabButton>
+      </Tab>
+      <DailyChart />
+      <WeeklyChart /> */}
+      <MonthlyChart type="월별" id={id} totalAmount={totalAmount} />
       <DummyBox />
     </ChartContainer>
   );
@@ -48,5 +78,9 @@ const DummyBox = styled.div`
   height: 200px;
   background: blueviolet;
 `;
+
+// const Tab = styled.div``;
+
+// const TabButton = styled.button``;
 
 export default Chart;
