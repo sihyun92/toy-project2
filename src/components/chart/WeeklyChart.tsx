9395b46cfa5ba3a517d47ec5434537d838bd6e60
pildrums <there7788@gmail.com>
2023-07-21 18:24:14 +0900

    init: :tada: 주별 차트 컴포넌트 작성 완료

--- src/components/chart/WeeklyChart.tsx
+++ src/components/chart/WeeklyChart.tsx
@@ -1,8 +1,41 @@
+import { useEffect, useState } from "react";
+import ApexCharts from "react-apexcharts";
+import { getPeriodConsume } from "../../lib/api/consumeAPI";
+import { IExpense } from "./Chart";
+
+// Component
 function WeeklyChart() {
+  const [totalAmount, setTotalAmount] = useState<number[]>([]);
+  const [id, setId] = useState<string[]>([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const result = await getPeriodConsume("weekly", "team1");
+        setTotalAmount(result.data.map((item: IExpense) => item.totalAmount));
+        setId(result.data.map((item: IExpense) => item._id));
+        console.log(result.data);
+        console.log(result.data.map((item: IExpense) => item.totalAmount));
+      } catch (e) {
+        console.error(e, "오류 발생!");
+      }
+    };
+    fetchData();
+  }, []);
+  // Render
   return (
-    <div>
-      <h1>주별 차트</h1>
-    </div>
+    <ApexCharts
+      series={totalAmount}
+      type="donut"
+      options={{
+        labels: id,
+        dataLabels: {
+          enabled: false,
+        },
+        title: {
+          text: "주별 차트",
+        },
+      }}
+    />
   );
 }
 
