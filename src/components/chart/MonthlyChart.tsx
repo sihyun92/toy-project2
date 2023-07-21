49b0cbf86a323ab7b9369db34b35ad5ee3bea077
pildrums <there7788@gmail.com>
2023-07-21 18:26:11 +0900

    chore: :wrench: 월별 차트 코드 정리 및 api 연동 문제 해결

--- src/components/chart/MonthlyChart.tsx
+++ src/components/chart/MonthlyChart.tsx
@@ -1,29 +1,38 @@
+import { useEffect, useState } from "react";
 import ApexCharts from "react-apexcharts";
-
-// Interface
-interface MonthlyChartProps {
-  id: string[];
-  totalAmount: number[];
-  type: string;
-}
+import { getPeriodConsume } from "../../lib/api/consumeAPI";
+import { IExpense } from "./Chart";
 
 // Component
-function MonthlyChart({ id, totalAmount, type }: MonthlyChartProps) {
+function MonthlyChart() {
+  const [totalAmount, setTotalAmount] = useState<number[]>([]);
+  const [id, setId] = useState<string[]>([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const result = await getPeriodConsume("monthly", "team1");
+        setTotalAmount(result.data.map((item: IExpense) => item.totalAmount));
+        setId(result.data.map((item: IExpense) => item._id));
+        console.log(result.data);
+        console.log(result.data.map((item: IExpense) => item.totalAmount));
+      } catch (e) {
+        console.error(e, "오류 발생!");
+      }
+    };
+    fetchData();
+  }, []);
   // Render
   return (
     <ApexCharts
       series={totalAmount}
-      type="bar"
+      type="donut"
       options={{
         labels: id,
         dataLabels: {
-          enabled: true,
-          formatter: (val: string) => {
-            return `${val}₩`;
-          },
+          enabled: false,
         },
         title: {
-          text: type,
+          text: '월별 차트',
         },
       }}
     />
